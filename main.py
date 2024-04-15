be8c9ae79b9b6f3c975c42865bc17d87e579310a
Aditya gupta <142041364+aditya-gupta-dev@users.noreply.github.com>
2024-04-15 17:11:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import argparse
 
+from handlers import handle_command
+
 def main():
     parser = argparse.ArgumentParser(description='Fetch content from a URL.')
     parser.add_argument('url', help='URL to fetch content from')
-    parser.add_argument('-t', help='Fetch only thumbnail')
+    parser.add_argument('-t', action='store_true', help='Fetch only thumbnail')
+    parser.add_argument('-b', action='store_true', help='Fetch the best stream')
+    parser.add_argument('-l', action='store_true', help='Fetch the lowest stream')
+    parser.add_argument('-i', action='store_true', help='get video info') # incomplete
+    parser.add_argument('-a', action='store_true', help='list every stream') # incomplete
 
     args = parser.parse_args()
 
-    print(args)
+    handle_command(args)
+
 
 if __name__ == "__main__":
     main()
