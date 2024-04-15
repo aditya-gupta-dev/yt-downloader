50cf3be277a3d9c19a410844d12b7bfd146f86c8
Aditya gupta <142041364+aditya-gupta-dev@users.noreply.github.com>
2024-04-15 17:11:00 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,1 +1,26 @@
+from threading import Thread
+from fetchers import get_best_format, get_lowest_format, get_thumbnail
+
+def handle_command(args):
+
+    if args.t:
+        Thread(
+            target=get_thumbnail,
+            args=(args.url, )
+        ).start()
+
+    elif args.b:
+        Thread(
+            target=get_best_format, 
+            args=(args.url, )
+            ).start()
+
+    elif args.l:
+        Thread(
+            target=get_lowest_format, 
+            args=(args.url, )
+            ).start()
+
+    else:
+        print('no arguments passed')
 
